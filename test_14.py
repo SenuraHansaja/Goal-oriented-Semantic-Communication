bab36f35c670ec58fda5cf6661b07003a8d27a3a
SenuraHansaja <87333867+SenuraHansaja@users.noreply.github.com>
2023-11-27 13:15:30 +0700

    accuracy part included
    this code include the accuracy of the model and optimizer and scheduler

--- test_14.py
+++ test_14.py
@@ -21,16 +21,17 @@ from torchvision.utils import make_grid
 from torchvision import models
 from torchsummary import summary
 from pytorch_lightning.loggers import CSVLogger
-
+from tqdm import tqdm
 
 
 logger = CSVLogger("logs_out", name="encoder_logs")
 
 
-batch_size = 128
+batch_size = 64
 num_workers = 8
-max_epochs = 100
-z_dim = 1024
+max_epochs = 200
+z_dim = 2048
+
 
 
 class BarlowTwinsTransform:
@@ -113,7 +114,7 @@ class BarlowTwinsLoss(nn.Module):
 
     def forward(self, z1, z2):
         # N is the batch size ,    D is output dim of projection head
-        z1_norm = (z1 - torch.mean(z1, dim=0)) / torch.std(z1, dim=0)
+        z1_norm = (z1 - torch.mean(z1, dim=0)) / torch.std(z1, dim=0) 
         z2_norm = (z2 - torch.mean(z2, dim=0)) / torch.std(z2, dim=0)
 
         cross_corr = torch.matmul(z1_norm.T, z2_norm) / self.batch_size
@@ -261,3 +262,96 @@ trainer = L.Trainer(
 
 
 trainer.fit(model, train_loader, val_loader)
+
+
+class classifire(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=2048, out_features=1024),
+            nn.ReLU(),
+            nn.Linear(in_features=1024, out_features=1024),
+            nn.ReLU(),
+            nn.Linear(in_features=1024, out_features=10),
+        )
+    
+    def forward(self, x:torch.Tensor):
+        x = self.block(x)
+        return x
+
+classifire_model = classifire()
+
+'''the below methods are the 2 implemented optimizer and schedulers 
+we can shift between them '''
+ai_optimizer = torch.optim.Adam(classifire_model.parameters(), lr = 1e-4)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(ai_optimizer, 200)
+goai_loss_fn = nn.CrossEntropyLoss()
+# ai_optimizer = torch.optim.SGD(classifire_model.parameters(), lr=1e-3,
+#                                 momentum=0.9, weight_decay=5e-4)
+# scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(ai_optimizer, 200)
+
+
+
+running_loss = 0.0
+
+running_corrects = 0
+epochs =  100
+
+for epoch in tqdm(range(epochs)):
+    running_loss = 0.0
+    running_corrects = 0
+    for batch in train_loader:
+        (img1, img2, _), label = batch
+        img1 = img1.to('cuda')
+        model = model.to('cuda')
+        label = label.to('cuda')
+        model.eval()
+        classifire_model.to('cuda')
+        classifire_model.train()
+
+        encoder_out = model.forward(img1)
+        input_to_goai = torch.unsqueeze(encoder_out, dim=1)             ### these unsuqeese is done to make the data follow NCHW pattern which is used inside the pytorch as a data format
+        input_to_goai = torch.unsqueeze(input_to_goai, dim=1)
+        #y_pred = GOAI_model(encoder_out)
+        
+       # GOAI_model.to('cuda')
+        y_pred = classifire_model(input_to_goai)
+        _, preds = torch.max(y_pred, 1)
+        
+        loss = goai_loss_fn(y_pred, label)
+        ai_optimizer.zero_grad()
+
+        loss.backward()
+
+        ai_optimizer.step()
+        running_loss += loss.item() * batch_size
+        running_corrects += torch.sum(preds == label.data)
+    scheduler.step()
+    epoch_loss = running_loss / len(train_loader.dataset)
+    epoch_acc = running_corrects.double() / len(train_loader.dataset)
+
+    print('Epoch: {} Loss: {:.4f} Acc: {:.4f}'.format(epoch,  epoch_loss, epoch_acc))
+
+
+
+correct = 0
+total = 0
+with torch.inference_mode():
+    for batch in val_loader:
+         (img1, img2, _), label = batch
+         model.eval()
+         classifire_model.eval()
+         model.to('cpu')
+         classifire_model.to('cpu')
+    
+         encoder_out = model.forward(img1)
+         input_to_goai = torch.unsqueeze(encoder_out, dim=1)             ### these unsuqeese is done to make the data follow NCHW pattern which is used inside the pytorch as a data format
+         input_to_goai = torch.unsqueeze(input_to_goai, dim=1)
+         y_pred = classifire_model(input_to_goai)
+         _, preds = torch.max(y_pred.data, 1)
+         total += label.size(0)
+         correct += (preds == label).sum().item()
+# epoch_acc = running_corrects.double() / len(val_loader.dataset)
+# print('Acc: {:.4f}'.format(epoch_acc))
+print('Accuracy of the network on the test images: %d %%' % (100 * correct / total))     
