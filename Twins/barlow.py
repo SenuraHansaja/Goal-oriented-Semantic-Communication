6bb83030029d4728af8cad638117d868fce7a154
SenuraHansaja <87333867+SenuraHansaja@users.noreply.github.com>
2023-11-27 23:13:34 +0700

    Add files via upload

---
+++ Twins/barlow.py
@@ -0,0 +1,162 @@
+from functools import partial
+from typing import Sequence, Tuple, Union
+
+import lightning as L
+import matplotlib.pyplot as plt
+import numpy as np
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+import torchvision.transforms.functional as VisionF
+from pytorch_lightning import Callback, LightningModule, Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
+from torch import Tensor
+from torch.utils.data import DataLoader
+from torchmetrics.functional import accuracy
+from torchvision.datasets import CIFAR10
+from torchvision.models.resnet import resnet34
+from torchvision.utils import make_grid
+from torchvision import models
+from torchsummary import summary
+from pytorch_lightning.loggers import CSVLogger
+
+
+'''
+below logger logs the on diagnoal and off diagonal loss to compare and contrast
+s
+'''
+logger = CSVLogger("logs_out", name="encoder_logs")  
+
+def fn(warmup_steps, step):
+    if step < warmup_steps:
+        return float(step) / float(max(1, warmup_steps))
+    else:
+        return 1.0
+
+
+## warm up steps to run the code
+def linear_warmup_decay(warmup_steps):
+    return partial(fn, warmup_steps)
+
+
+
+
+## loss function 
+class BarlowTwinsLoss(nn.Module):
+    def __init__(self, batch_size, lambda_coeff=5e-3, z_dim=128):
+        super().__init__()
+
+        self.z_dim = z_dim
+        self.batch_size = batch_size
+        self.lambda_coeff = lambda_coeff
+
+    def off_diagonal_ele(self, x):
+        # taken from: https://github.com/facebookresearch/barlowtwins/blob/main/main.py
+        # return a flattened view of the off-diagonal elements of a square matrix
+        n, m = x.shape
+        assert n == m
+        return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
+
+    def forward(self, z1, z2):
+        # N x D, where N is the batch size and D is output dim of projection head
+        z1_norm = (z1 - torch.mean(z1, dim=0)) / torch.std(z1, dim=0)
+        z2_norm = (z2 - torch.mean(z2, dim=0)) / torch.std(z2, dim=0)
+
+        cross_corr = torch.mm(z1_norm.T, z2_norm) / self.batch_size
+
+        on_diag = torch.diagonal(cross_corr).add_(-1).pow_(2).sum()
+        off_diag = self.off_diagonal_ele(cross_corr).pow_(2).sum()
+
+        return on_diag + self.lambda_coeff * off_diag, on_diag, off_diag
+
+
+
+
+
+## encoder model
+class BarlowTwins(LightningModule):
+    def __init__(
+        self,
+        encoder,
+        encoder_out_dim,
+        num_training_samples,
+        batch_size,
+        lambda_coeff=5e-3,
+        z_dim=128,
+        learning_rate=1e-4,
+        warmup_epochs=10,
+        max_epochs=200,
+    ):
+        super().__init__()
+
+        self.encoder = encoder
+        self.projection_head = ProjectionHead(input_dim=encoder_out_dim, hidden_dim=encoder_out_dim, output_dim=z_dim)
+        self.loss_fn = BarlowTwinsLoss(batch_size=batch_size, lambda_coeff=lambda_coeff, z_dim=z_dim)
+
+        self.learning_rate = learning_rate
+        self.warmup_epochs = warmup_epochs
+        self.max_epochs = max_epochs
+
+        self.train_iters_per_epoch = num_training_samples // batch_size
+
+    def forward(self, x):
+        return self.encoder(x)
+
+    def shared_step(self, batch):
+        (x1, x2, _), _ = batch
+
+        z1 = self.projection_head(self.encoder(x1))
+        z2 = self.projection_head(self.encoder(x2))
+
+        loss, on_diag, off_diag= self.loss_fn(z1, z2)
+       
+        self.log("on_diag", on_diag.sum(), on_step=True, on_epoch=True, prog_bar= True, logger=logger)
+        self.log("off_diag", off_diag.sum(), on_step=True, on_epoch=True, prog_bar= True, logger=logger)
+
+        return loss
+    
+    def training_step(self, batch, batch_idx):
+        loss = self.shared_step(batch)
+        self.log("train_loss", loss, on_step=True, on_epoch=False)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss = self.shared_step(batch)
+        self.log("val_loss", loss, on_step=False, on_epoch=True)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+
+        warmup_steps = self.train_iters_per_epoch * self.warmup_epochs
+
+        scheduler = {
+            "scheduler": torch.optim.lr_scheduler.LambdaLR(
+                optimizer,
+                linear_warmup_decay(warmup_steps),
+            ),
+            "interval": "step",
+            "frequency": 1,
+        }
+
+        return [optimizer], [scheduler]
+    
+
+
+
+
+## projection head model
+class ProjectionHead(nn.Module):
+    def __init__(self, input_dim=1024, hidden_dim=1024, output_dim=1024):
+        super().__init__()
+
+        self.projection_head = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim, bias=True),
+            nn.BatchNorm1d(hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, output_dim, bias=False),
+        )
+
+    def forward(self, x):
+        return self.projection_head(x)
